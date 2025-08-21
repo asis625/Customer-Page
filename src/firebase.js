d4dabb589b429d09b2a709ec45fab380cdbe8fd8
Ashish Sapkota <s357624@students.cdu.edu.au>
2025-08-21 04:35:14 +0000

    Save my current progress

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Shared Firebase project: Nehu Thrifts and Trends
+
 const firebaseConfig = {
   apiKey: "AIzaSyBDjuDsA1OYHEV6QaFv-f-6NVf3RGno_u8",
   authDomain: "nehu-thrifts-and-trends.firebaseapp.com",
   projectId: "nehu-thrifts-and-trends",
-  storageBucket: "nehu-thrifts-and-trends.appspot.com",
+  storageBucket: "nehu-thrifts-and-trends.firebasestorage.app",
   messagingSenderId: "835828658610",
-  appId: "1:835828658610:web:82aa910d1368cf1352b4ab"
+  appId: "1:835828658610:web:82aa910d1368cf1352b4ab",
+  measurementId: "G-CSTW8D4GQY"
 };
 
 const app = initializeApp(firebaseConfig);
